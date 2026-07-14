ba51220accb08e8fbcf3d355f5064ac22cd29094
Dhanaba_raju <88547771+Dhana1712@users.noreply.github.com>
2026-07-14 10:27:39 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun singleNumber(nums: IntArray): Int {
+        var result = nums[0]
+        for (i in 1 until nums.size) {
+            result = result xor nums[i]
+        }
+        return result
+    }
+}
